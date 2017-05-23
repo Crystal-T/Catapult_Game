a9aadd446ae3aa1c6f92ce5efc4c04330ddbb87f
Crystal-T <cthurley13@hotmail.com>
2017-05-23 17:38:13 +1000

    first commit

---
+++ CAR/Assets/Scripts/Shooting.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour {
+
+	public bool isPressed;
+	public GameObject bulletPrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		isPressed = Input.GetKey (KeyCode.Mouse0);
+	}
+
+	void FixedUpdate() {
+		if (isPressed == true) {
+			Instantiate (bulletPrefab, transform.position, transform.rotation);
+		}
+	}
+
+	private void OnCollision(Collision collidedObject) {
+		
+
+
+		}
+ 
+
+	//check tag for "Win"
+	//print some win message
+}
